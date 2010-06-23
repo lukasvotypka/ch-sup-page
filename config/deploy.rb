4bbb875327242d444f6012cdefd70fa1665e9d05
Lukas <lukas.votypka@gmail.com>
2010-06-23 11:19:45 +0200

    uprava countdown-u

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,29 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "chatsupport"
+set :repository, "git://github.com/luigi-sk/ch-sup-page.git"
+set :scm, "git"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+role :web, "server5.railshosting.cz"
+role :app, "server5.railshosting.cz"
+role :db,  "server5.railshosting.cz", :primary => true
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :deploy_to, "/home/chatsupport/app/"
+set :user, "chatsupport"
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :use_sudo, false
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+task :after_update_code, :roles => [:app, :db] do
+  run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+end
+
+
+namespace :deploy do
+  task :start, :roles => :app do
+  end
+end
+
+namespace :deploy do
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
