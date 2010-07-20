bcc22d31ef82de6cee93533c5fd5ed84edd51fc3
Lukas <lukas.votypka@gmail.com>
2010-07-20 15:47:08 +0200

    locales premiestnene do application controlleru

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,31 @@ class ApplicationController < ActionController::Base
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
   
+  before_filter :set_locale
+  
+  def available_locales; AVAILABLE_LOCALES; end
+  
+  def set_locale
+    I18n.locale = extract_locale_from_tld
+    @server_domain = request.host.split('.')
+    @server_domain.pop
+  end
+  
+  # Get locale from top-level domain or return nil if such locale is not available
+  # You have to put something like:
+  #   127.0.0.1 application.com
+  #   127.0.0.1 application.it
+  #   127.0.0.1 application.pl
+  # in your /etc/hosts file to try this out locally
+  def extract_locale_from_tld
+    parsed_locale = request.host.split('.').last
+    (available_locales.include? parsed_locale) ? parsed_locale  : nil
+  end
+  
+  def get_locale_mark
+    I18n.locale.to_s
+  end
+  
   protected
     def current_user_session
       return @current_user_session if defined?(@current_user_session)
--- app/controllers/public_web_controller.rb
+++ app/controllers/public_web_controller.rb
@@ -1,27 +1,4 @@
 class PublicWebController < ApplicationController
-  before_filter :set_locale
   
-  def available_locales; AVAILABLE_LOCALES; end
-  
-  def set_locale
-    I18n.locale = extract_locale_from_tld
-    @server_domain = request.host.split('.')
-    @server_domain.pop
-  end
-  
-  # Get locale from top-level domain or return nil if such locale is not available
-  # You have to put something like:
-  #   127.0.0.1 application.com
-  #   127.0.0.1 application.it
-  #   127.0.0.1 application.pl
-  # in your /etc/hosts file to try this out locally
-  def extract_locale_from_tld
-    parsed_locale = request.host.split('.').last
-    (available_locales.include? parsed_locale) ? parsed_locale  : nil
-  end
-  
-  def get_locale_mark
-    I18n.locale.to_s
-  end
   
 end
