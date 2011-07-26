b248a9078126be69ecfb7430d8205fdbf78b1c6d
Lukas <lukas.votypka@gmail.com>
2011-07-27 01:59:25 +0200

    prices

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function order(type) {
+    location.href='https://support.chat-support.net/registration_requests/new?registration_request[subscription_type]='+type;
+}
